3d73257ca4e2b49b2d8a4c02425cebab1db12f74
MilenaSchmidt <milenaschmidt@protonmail.com>
2019-12-27 17:52:49 +0100

    AdminModule# Admin model and migration

---
+++ Modules/Admin/Database/Migrations/2019_12_27_165018_create_admin_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username', 50);
+            $table->string('email', 150)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin');
+    }
+}
