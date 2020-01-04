d8232d385e46307a271a09d3e040b9127ad92d60
MilenaSchmidt <milenaschmidt@protonmail.com>
2020-01-04 18:05:40 +0100

    Modules/Basket BasketProducts Pivot

---
+++ Modules/Basket/Providers/BasketServiceProvider.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Modules\Basket\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
+
+class BasketServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerTranslations();
+        $this->registerConfig();
+        $this->registerViews();
+        $this->registerFactories();
+        $this->loadMigrationsFrom(module_path('Basket', 'Database/Migrations'));
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([
+            module_path('Basket', 'Config/config.php') => config_path('basket.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            module_path('Basket', 'Config/config.php'), 'basket'
+        );
+    }
+
+    /**
+     * Register views.
+     *
+     * @return void
+     */
+    public function registerViews()
+    {
+        $viewPath = resource_path('views/modules/basket');
+
+        $sourcePath = module_path('Basket', 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ],'views');
+
+        $this->loadViewsFrom(array_merge(array_map(function ($path) {
+            return $path . '/modules/basket';
+        }, \Config::get('view.paths')), [$sourcePath]), 'basket');
+    }
+
+    /**
+     * Register translations.
+     *
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        $langPath = resource_path('lang/modules/basket');
+
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, 'basket');
+        } else {
+            $this->loadTranslationsFrom(module_path('Basket', 'Resources/lang'), 'basket');
+        }
+    }
+
+    /**
+     * Register an additional directory of factories.
+     *
+     * @return void
+     */
+    public function registerFactories()
+    {
+        if (! app()->environment('production') && $this->app->runningInConsole()) {
+            app(Factory::class)->load(module_path('Basket', 'Database/factories'));
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
