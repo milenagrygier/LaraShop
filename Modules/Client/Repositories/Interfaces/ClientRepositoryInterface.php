efc9a99decb7c24cc105c96f6865a9309c51e461
MilenaSchmidt <milenaschmidt@protonmail.com>
2020-01-21 18:40:14 +0100

    Modules/Client# Client Repository and Repository Interface

---
+++ Modules/Client/Repositories/Interfaces/ClientRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Client\Repositories\Interfaces;
+
+use Modules\Client\Entities\Client;
+use Illuminate\Database\Eloquent\Builder;
+
+interface ClientRepositoryInterface
+{
+    public function find(int $id): ?Client;
+
+    public function getDataTableBuilder(): Builder;
+
+    public function create(array $data): Client;
+
+    public function update(array $data, int $id): bool;
+
+    public function delete(int $id): bool;
+}
