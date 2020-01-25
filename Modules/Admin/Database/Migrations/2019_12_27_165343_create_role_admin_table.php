8debda93403ee2980e9f8cd12e8da1b4177c25d6
MilenaSchmidt <milenaschmidt@protonmail.com>
2020-01-25 17:20:11 +0100

    Modules\Admin Role_admin Pivot table

--- Modules/Admin/Database/Migrations/2019_12_27_165343_create_role_admin_table.php
+++ Modules/Admin/Database/Migrations/2019_12_27_165343_create_role_admin_table.php
@@ -14,9 +14,11 @@ class CreateRoleAdminTable extends Migration
     public function up()
     {
         Schema::create('role_admin', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('admin_id');
-            $table->integer('role_id');
+            $table->integer('admin_id')->unsigned()->index();
+            $table->foreign('admin_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('role_id')->unsigned()->index();
+            $table->foreign('role_id')->references('id')->on('images')->onDelete('cascade');
+            $table->primary(['admin_id', 'role_id']);
         });
     }
 
