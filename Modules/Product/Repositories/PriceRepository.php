d8cee03936caf3e2b44d82e0f9daf91b4fdda85a
MilenaSchmidt <milenaschmidt@protonmail.com>
2020-01-27 22:14:53 +0100

    Modules/Product Price repository and repository interface

---
+++ Modules/Product/Repositories/PriceRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Product\Repositories;
+
+use Modules\Product\Repositories\Interfaces\ProductRepositoryInterface;
+
+class ProductRepository implements ProductRepositoryInterface
+{
+
+}
