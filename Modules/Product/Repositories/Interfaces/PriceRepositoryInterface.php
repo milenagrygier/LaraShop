9d79b3fece33d58ae298a84f000e11ebc8bac2bf
MilenaSchmidt <milenaschmidt@protonmail.com>
2020-01-28 20:49:00 +0100

    Modules/Product Price repository and repository interface methods

--- Modules/Product/Repositories/Interfaces/PriceRepositoryInterface.php
+++ Modules/Product/Repositories/Interfaces/PriceRepositoryInterface.php
@@ -2,7 +2,15 @@
 
 namespace Modules\Product\Repositories\Interfaces;
 
+use Modules\Product\Entities\Price;
+
 interface PriceRepositoryInterface
 {
+    public function find(int $id): ?Price;
 
+    public function create(array $data): Price;
+
+    public function update(array $data, int $id): bool;
+
+    public function delete(int $id): bool;
 }
--- Modules/Product/Repositories/PriceRepository.php
+++ Modules/Product/Repositories/PriceRepository.php
@@ -3,8 +3,27 @@
 namespace Modules\Product\Repositories;
 
 use Modules\Product\Repositories\Interfaces\ProductRepositoryInterface;
+use Modules\Product\Entities\Price;
 
 class ProductRepository implements ProductRepositoryInterface
 {
+    public function find(int $id): ?Price
+    {
+        return Price::find($id);
+    }
 
+    public function create(array $data): Price
+    {
+        return Price::create($data);
+    }
+
+    public function update(array $data, int $id): bool
+    {
+        return $this->find($id)->update($data);
+    }
+
+    public function delete(int $id): bool
+    {
+        return $this->find($id)->delete();
+    }
 }
