19a8564dec6431bd364f8abe651cfc80e8a0a0f2
MilenaSchmidt <milenaschmidt@protonmail.com>
2020-02-04 19:08:25 +0100

    Modules/Order Order repository and repository interface

---
+++ Modules/Order/Repositories/Interfaces/OrderRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Order\Repositories\Interfaces;
+
+use Modules\Order\Entities\Order;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Schema\Builder;
+
+interface OrderRepositoryInterface
+{
+    public function find(int $id): ?Order;
+
+    public function findByClient(int $client_id): Collection;
+
+    public function getDataTableBuilder(): Builder;
+
+    public function update(array $data, int $id): bool;
+
+    public function delete(int $id): bool;
+}
