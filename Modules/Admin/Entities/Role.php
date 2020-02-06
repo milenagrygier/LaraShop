e0bae6f53b98f616b14c8b6e0af86f2f6c6ca215
MilenaSchmidt <milenaschmidt@protonmail.com>
2020-02-06 19:47:19 +0100

    Modules\Admin Role declaration strict types and property list

--- Modules/Admin/Entities/Role.php
+++ Modules/Admin/Entities/Role.php
@@ -1,9 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Admin\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property int $level
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 class Role extends Model
 {
     /**
