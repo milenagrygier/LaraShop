7106adcba31dbe0ce9fdf9d214913a138a5c99fe
MilenaSchmidt <milenaschmidt@protonmail.com>
2020-02-06 19:55:27 +0100

    Modules\Basket Basket strict types declaration and property list

--- Modules/Basket/Entities/Basket.php
+++ Modules/Basket/Entities/Basket.php
@@ -1,10 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Basket\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $client_id
+ */
 class Basket extends Model
 {
-    protected $fillable = [];
+    protected $fillable = [
+        'client_id'
+    ];
 }
