03cffb0600074c3394e7c034d4e40dac12e050fa
MilenaSchmidt <milenaschmidt@protonmail.com>
2020-02-07 20:46:15 +0100

    Modules\Logs strict types declaration and Log property list

--- Modules/Logs/Entities/Log.php
+++ Modules/Logs/Entities/Log.php
@@ -1,9 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Logs\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\HasEncryptableAttributes;
+use Carbon\Carbon;
+
+/**
+ * @property int $id
+ * @property string $instance
+ * @property string $channel
+ * @property string $file
+ * @property int $line
+ * @property string $exception_class
+ * @property string $ip
+ * @property string $user_agent
+ * @property int $level
+ * @property string $level_name
+ * @property string $message
+ * @property string $context
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 
 class Log extends Model
 {
