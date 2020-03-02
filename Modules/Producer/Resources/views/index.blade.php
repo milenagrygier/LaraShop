a6045b3a80dc2cfc20533bbd5fbd4be50a33d649
Milena Schmidt <ms@nxo.pl>
2020-03-02 11:15:15 +0100

    Modules\Producer Module init

---
+++ Modules/Producer/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('producer::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('producer.name') !!}
+    </p>
+@endsection
